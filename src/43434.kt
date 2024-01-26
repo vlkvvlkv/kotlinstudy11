ba9021b5bca024ffbb5dcca72f1bca6e7c97b6cc
student <student@C313-3>
2024-01-26 09:59:45 +0300

    q

--- src/43434.kt
+++ src/43434.kt
@@ -1,12 +1,15 @@
 fun main() {
+    main1()
+    task2()
+}
+fun main1(){
     val t: Int = readLine()!!.toInt()
     val e: String = readLine()!!
     val x: Int = readLine()!!.toInt()
-calculate(t,x,e)
-
+    calculate(t,x,e)
 }
-fun calculate(num1: Int, num2: Int, operand: String){
-    when(operand) {
+fun calculate(num1: Int, num2: Int, operand: String) {
+    when (operand) {
         "+" -> {
             var r = num1 + num2
             print("$num1 + $num2 = ${num1 + num2}")
@@ -16,5 +19,17 @@ fun calculate(num1: Int, num2: Int, operand: String){
         "/" -> print("$num1 + $num2 = ${num1 / num2}")
         else -> print("Введено неккоректное число")
     }
+}
+fun task2() {
+    val list: List<Int> = listOf(23, 34)
+    val numberlist: MutableList<Int> = mutableListOf()
+    numberlist.add(33)
+    val eq: Map<Int, Int> = mapOf()
+    val rq: HashMap<Int, Int> = hashMapOf()
+}
+fun main2() {
 
 }
+
+
+    }
