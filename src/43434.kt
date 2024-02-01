b11668cffe6fa23df0edbb8c3380621e726b6409
student <student@C313-3>
2024-02-01 18:28:21 +0300

    qq

--- src/43434.kt
+++ src/43434.kt
@@ -1,13 +1,29 @@
+import java.lang.Exception
+
 fun main() {
     main1()
     task2()
 }
-fun main1(){
-    val t: Int = readLine()!!.toInt()
-    val e: String = readLine()!!
-    val x: Int = readLine()!!.toInt()
-    calculate(t,x,e)
+
+fun main1() {
+    while (true) {
+    try {
+        val t: Int = readLine()!!.toInt()
+        val e: String = readLine()!!
+        val x: Int = readLine()!!.toInt()
+        calculate(t, x, e)
+    } catch (e: NumberFormatException) {
+        print("Введено не число")
+    } catch (e: Exception) {
+
+    }
+        print(", продолжить? ")
+        var p = readLine()
+        if(p == "нет")
+            break
 }
+}
+
 fun calculate(num1: Int, num2: Int, operand: String) {
     when (operand) {
         "+" -> {
@@ -20,6 +36,7 @@ fun calculate(num1: Int, num2: Int, operand: String) {
         else -> print("Введено неккоректное число")
     }
 }
+
 fun task2() {
     val list: List<Int> = listOf(23, 34)
     val numberlist: MutableList<Int> = mutableListOf()
@@ -27,9 +44,9 @@ fun task2() {
     val eq: Map<Int, Int> = mapOf()
     val rq: HashMap<Int, Int> = hashMapOf()
 }
+
 fun main2() {
 
 }
 
 
-    }
